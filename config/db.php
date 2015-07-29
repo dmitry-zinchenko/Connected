7a04eca3a9768ae6ece146207a79312fc2730728
OxanaVasilieva <Oxviolet@yandex.ru>
2015-07-29 12:01:08 +0600

    test

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=connected',
-    'username' => 'connected',
-    'password' => 'connected',
-    'charset' => 'utf8',
-];
