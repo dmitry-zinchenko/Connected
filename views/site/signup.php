72eb060902bb92edd8f6770818818a0bec782b03
Dmitry Zinchenko <zinch.dm@gmail.com>
2015-07-30 11:59:33 +0600

    Some commented code deleted

--- views/site/signup.php
+++ views/site/signup.php
@@ -33,35 +33,3 @@ $this->title = Html::encode('Connected - ') . Yii::t('app', 'Sign up');
         <?php ActiveForm::end() ?>
     </div>
 </div>
-
-
-<!--<div class="site-login">-->
-<!--    <div class="login-form-back">-->
-<!--        <h1>--><?//= Html::encode($this->title) ?><!--</h1>-->
-<!---->
-<!--        --><?php //$form = ActiveForm::begin([
-//            'id' => 'login-form',
-//            'options' => ['class' => 'form-horizontal'],
-//            'fieldConfig' => [
-//                'template' => "{label}\n<div class=\"\">{input}</div>\n<div class=\"\">{error}</div>",
-//                'labelOptions' => ['class' => 'control-label'],
-//            ],
-//        ]); ?>
-<!---->
-<!--        --><?//= $form->field($model, 'username') ?>
-<!---->
-<!--        --><?//= $form->field($model, 'password')->passwordInput() ?>
-<!---->
-<!--        --><?//= $form->field($model, 'rememberMe', [
-//            'template' => "<div class=\"\">{input}</div>\n<div class=\"\">{error}</div>",
-//        ])->checkbox() ?>
-<!---->
-<!--        <div class="form-group">-->
-<!--            <div class="">-->
-<!--                --><?//= Html::submitButton('Sign in', ['class' => 'btn btn-primary login-button', 'name' => 'login-button']) ?>
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        --><?php //ActiveForm::end(); ?>
-<!--    </div>-->
-<!--</div>-->
